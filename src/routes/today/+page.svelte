21f54697ad496da58c1c9bbe19bdd5e19f9325c1
Trinadh <53462309+tr1nadh@users.noreply.github.com>
2026-04-27 21:20:10 +0530

    Some fixes

--- src/routes/today/+page.svelte
+++ src/routes/today/+page.svelte
@@ -6,6 +6,7 @@
 	import QuickCaptureModal from '$lib/components/QuickCaptureModal.svelte';
 	import SubtleHeader from '$lib/components/SubtleHeader.svelte';
 	import TaskRow from '$lib/components/TaskRow.svelte';
+	import TimeBlock from '$lib/components/TimeBlock.svelte';
 	import UpcomingPanel from '$lib/components/UpcomingPanel.svelte';
 	import ZenProgress from '$lib/components/ZenProgress.svelte';
 	import {
@@ -1588,55 +1589,17 @@
 
 							{#if timelineBlocks.length}
 								{#each timelineBlocks as block (block.id)}
-									<div
-										class={`today-time-block ${block.active ? 'active' : ''} ${selectedBlockId === block.id ? 'selected' : ''} ${block.collision ? 'collision' : ''} ${timeBlockDragState?.blockId === block.id ? 'dragging' : ''}`}
-										style={`top:${block.top}%; height:${block.height}%; cursor: ${timeBlockDragState?.blockId === block.id ? 'grabbing' : 'grab'}; --block-color: ${block.color};`}
-										onpointerdown={(event) => startTimeBlockDrag(event, block)}
-										onclick={(event) => handleBlockClick(event, block.id)}
-										role="button"
-										tabindex="0"
-										onkeydown={(e) => e.key === 'Enter' && handleBlockClick(e, block.id)}
-									>
-										{#if selectedBlockId === block.id}
-											<button
-												class="today-time-block-delete"
-												type="button"
-												title="Delete block"
-												onclick={(e) => { e.stopPropagation(); removeModeTimeBlock(block.id); }}
-											>
-												<i class="fa-solid fa-trash-can"></i>
-											</button>
-										{/if}
-
-											<button
-												class="today-time-block-handle top"
-												type="button"
-												aria-label={`Resize ${block.mode} block start`}
-												onpointerdown={(event) => startTimeBlockResize(event, block, 'start')}
-											></button>
-
-											<div class="today-time-block-mode">
-												{block.mode} 
-												<span class="ms-1 opacity-50 pe-none" style="font-size: 0.75em;">({block.durationDisplay})</span>
-											</div>
-
-											<div class="today-time-block-range">
-												{formatTimeLabel(block.startTime)} to {formatTimeLabel(block.endTime)}
-											</div>
-
-
-											{#if block.active}
-												<div class="today-time-block-progress" style={`width: ${block.progress}%;`}></div>
-											{/if}
-
-											<button
-												class="today-time-block-handle bottom"
-												type="button"
-												aria-label={`Resize ${block.mode} block end`}
-												onpointerdown={(event) => startTimeBlockResize(event, block, 'end')}
-											></button>
-										</div>
-									{/each}
+									<TimeBlock
+										{block}
+										{selectedBlockId}
+										{timeBlockDragState}
+										{formatTimeLabel}
+										startTimeBlockDrag={startTimeBlockDrag}
+										handleBlockClick={handleBlockClick}
+										removeModeTimeBlock={removeModeTimeBlock}
+										startTimeBlockResize={startTimeBlockResize}
+									/>
+								{/each}
 								{:else}
 									<div class="today-time-empty">
 										<i class="fa-regular fa-clock"></i>
